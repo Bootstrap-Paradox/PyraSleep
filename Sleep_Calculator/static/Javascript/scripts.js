2b210d09672489daa72af53e542dd13c18a03a29
sid Kuchnure <sidkuchnure@gmail.com>
2021-01-04 22:27:51 +0530

    Made Changes to the Calculate Page

--- Sleep_Calculator/static/Javascript/scripts.js
+++
@@ -1,8 +0,0 @@
-function calculte() {
- var time = document.getElementById('time').value;
- let res = document.getElementById('time-section');
- if (res == null){
-   time = 1;
- }
- res.innerHTML = parseInt(time) + 1.5;
-}
